3714dd716b38c72297920a8bca7032ad4f466d97
littleJamJar <j.h.wikaira@gmail.com>
2016-07-15 13:33:17 +1200

    Initial blank project

---
+++ ThingsToDo/ThingsToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThingsToDo
+//
+//  Created by james wikaira on 15/07/16.
+//  Copyright © 2016 james wikaira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
