2efeb22cf948100768cf20336045116a920e03ce
littleJamJar <j.h.wikaira@gmail.com>
2016-07-16 11:39:18 +1200

    Add slide transition between tabs (scrolls right only)

--- ThingsToDo/ThingsToDo/TransitioningObject.swift
+++ ThingsToDo/ThingsToDo/TransitioningObject.swift
@@ -14,91 +14,65 @@ import UIKit
 class TransitioningObject: NSObject, UIViewControllerAnimatedTransitioning {
     
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        // Get references for both views
         let fromView: UIView = transitionContext.viewForKey(UITransitionContextFromViewKey)!
         let toView: UIView = transitionContext.viewForKey(UITransitionContextToViewKey)!
         
-        //transitionContext.containerView()!.addSubview(fromView)
-        //transitionContext.containerView()!.addSubview(toView)
+        //let tabBarController = TabBarViewController()
         
-        let option = UIViewAnimationOptions.TransitionFlipFromLeft
-        
-        UIView.transitionFromView(fromView, toView: toView, duration: transitionDuration(transitionContext), options: option) { finished in
-            transitionContext.completeTransition(true)
+        guard let superView = fromView.superview else {
+            print("Error: Could not find super view..")
+            return
         }
-    }
+        
+        // Add to view to the super view
+        superView.addSubview(toView)
+        
+        // Get the frame size of the fromView
+        let viewSize: CGRect = fromView.frame
+        
+        //    BOOL scrollRight = controllerIndex > tabBarController.selectedIndex;
+
+        // Determine whether we are scrolling right or left.
+        let scrollRight = true
+
+        // Position the 'toView' off the screen to the right.
+        toView.frame = CGRectMake((scrollRight ? 320 : -320), viewSize.origin.y, 320, viewSize.size.height)
+        
+        // This function deals with the actual transition.
+        UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0.1, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            
+                //  Function argument breakdown
+                //  - delay                     -> Amount of time in seconds before the transition starts
+                //  - usingSpringWithDamping    -> Amount the screen 'bounces'. The higher the number the greater the damping, i.e. the less the 'bounce'
+                //  - initialSpringVelocity     -> Does seem to make much of a difference unless set to very high numbers.
+                //  - options                   -> This doesn't seem to make a difference as it never gets used. Have experimented with changing it and it shows no difference. This code below makes use of the 'options' argument. It doesn't allow the push style transition we're after, though. Will keep it here for reference.
+                //  EXAMPLE: 
+                        //UIView.transitionFromView(fromView, toView: toView, duration: self.transitionDuration(transitionContext), options:                                                                          UIViewAnimationOptions.TransitionCrossDissolve) { finished in transitionContext.completeTransition(true)}
+            
+            
+                fromView.frame = CGRectMake((scrollRight ? -320 : 320), viewSize.origin.y, 320, viewSize.size.height);
+                toView.frame = CGRectMake(0, viewSize.origin.y, 320, viewSize.size.height);
+            
+            }) { (finished) in
+                //fromView.removeFromSuperview()    //FIXME: This doesn't seem to do anything. Why is it here?
+                transitionContext.completeTransition(finished)
+        }
+     }
     
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
-        return 0.25
+        return 0.5
     }
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+    
+    // return the animator used when dismissing from a viewcontroller
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+
 }
 
-//
-//// MARK: UIViewControllerAnimatedTransitioning protocol methods
-//
-//// animate a change from one viewcontroller to another
-//func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-//    
-//    // get reference to our fromView, toView and the container view that we should perform the transition in
-//    let container = transitionContext.containerView()!
-//    let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey)!
-//    let toView = transitionContext.viewForKey(UITransitionContextToViewKey)!
-//    
-//    // set up from 2D transforms that we'll use in the animation
-//    let offScreenRight = CGAffineTransformMakeTranslation(container.frame.width, 0)
-//    let offScreenLeft = CGAffineTransformMakeTranslation(-container.frame.width, 0)
-//    
-//    // start the toView to the right of the screen
-//    //toView.transform = offScreenRight
-//    toView.transform = offScreenLeft
-//    
-//    
-//    // add the both views to our view controller
-//    container.addSubview(toView)
-//    container.addSubview(fromView)
-//    
-//    // get the duration of the animation
-//    // DON'T just type '0.5s' -- the reason why won't make sense until the next post
-//    // but for now it's important to just follow this approach
-//    let duration = self.transitionDuration(transitionContext)
-//    
-//    // perform the animation!
-//    // for this example, just slid both fromView and toView to the left at the same time
-//    // meaning fromView is pushed off the screen and toView slides into view
-//    // we also use the block animation usingSpringWithDamping for a little bounce
-//    UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.LayoutSubviews, animations: {
-//        
-//        //fromView.transform = offScreenLeft
-//        
-//        fromView.transform = offScreenRight
-//        
-//        toView.transform = CGAffineTransformIdentity
-//        
-//        }, completion: { finished in
-//            
-//            // tell our transitionContext object that we've finished animating
-//            transitionContext.completeTransition(true)
-//            
-//    })
-//    
-//    
-//    
-//}
-//
-//// return how many seconds the transiton animation will take
-//func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
-//    return 0.5
-//}
-//
-//// MARK: UIViewControllerTransitioningDelegate protocol methods
-//
-//// return the animator when presenting a viewcontroller
-//// remember that an animator (or animation controller) is any object that aheres to the UIViewControllerAnimatedTransitioning protocol
-//func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//    return self
-//}
-//
-//// return the animator used when dismissing from a viewcontroller
-//func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//    return self
-//}
-//
